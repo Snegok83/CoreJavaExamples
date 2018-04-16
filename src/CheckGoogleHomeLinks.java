867683ea23e4903ba49ab35fd20070b407952bd4
Snegok83 <38438492+Snegok83@users.noreply.github.com>
2018-04-16 16:24:43 -0500

    2-nd commit

--- src/CheckGoogleHomeLinks.java
+++ src/CheckGoogleHomeLinks.java
@@ -8,29 +8,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CheckGoogleHomeLinks {
 
-		
-		private static String driverType="webdriver.chrome.driver"; 
-		private static String pathToDriver="C:\\tmp\\chromedriver.exe";
-		public static WebDriver driver;
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
+	private static String driverType = "webdriver.chrome.driver";
+	private static String pathToDriver = "C:\\tmp\\chromedriver.exe";
+	public static WebDriver driver;
 
-			System.setProperty(driverType, pathToDriver);
-			 driver = new ChromeDriver();
-			 driver.get("https://google.com");
-			 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			
-		
-			List<WebElement> links = driver.findElements(By.tagName("a"));
-			System.out.println(links.size());
-			
-			for (WebElement e : links) {
-				String checkLinkName = e.getText();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty(driverType, pathToDriver);
+		driver = new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+
+		for (WebElement e : links) {
+			String checkLinkName = e.getText();
 			System.out.println(checkLinkName);
-			
-			}
-			driver.get("https://google.com");
-			driver.close();
+
+		}
+		driver.get("https://google.com");
+		driver.close();
 	}
 
 }
