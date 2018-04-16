79ba9f4a43e22a5565527eb144a89afc8efffc57
Snegok83 <38438492+Snegok83@users.noreply.github.com>
2018-04-16 17:04:48 -0500

    formating text

--- src/testNG/SimpleConnectionExampleWithTestNG.java
+++ src/testNG/SimpleConnectionExampleWithTestNG.java
@@ -13,43 +13,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class SimpleConnectionExampleWithTestNG {
- 
-	
+
 	private static String driverType = "webdriver.chrome.driver";
 	private static String pathToDriver = "C:\\tmp\\chromedriver.exe";
 	private static WebDriver driver;
-  
+
 	@BeforeTest
-  public void beforeTest() {
-  
+	public void beforeTest() {
+
 		System.setProperty(driverType, pathToDriver);
-		
+
 		driver = new ChromeDriver();
 		driver.get("https://google.com");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
-  }
 
-  
-  @Test
-  public void f() {
-	  
-	  List<WebElement> links = driver.findElements(By.tagName("a"));
-		//Print number of found links from a page
+	}
+
+	@Test
+	public void f() {
+
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		// Print number of found links from a page
 		System.out.println(links.size());
-		//taking names of links 
+		// taking names of links
 		for (WebElement e : links) {
 			String checkLinkName = e.getText();
 			System.out.println(checkLinkName);
 
 		}
-  
-  }
-  
-  
-  @AfterTest
-  public void afterTest() {
-	  driver.close();
-  }
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+	}
 
 }
